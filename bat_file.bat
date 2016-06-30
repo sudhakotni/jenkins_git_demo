90c6c1cad6c74ced1030e5d02a4e921538303141
sudhakotni <sudha.kotni@innroad.com>
2016-06-30 14:53:30 +0530

    sds

--- bat_file.bat
+++ bat_file.bat
@@ -1,2 +1,2 @@
 C:
-node C:/Users/sudha.kotni/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -H output.html
+node C:/Users/sudha.kotni/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -t output.XML
