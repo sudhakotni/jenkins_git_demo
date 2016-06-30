dfad8c23f05e6516307761fa53d652a9f824c42a
sudhakotni <sudha.kotni@innroad.com>
2016-06-30 14:39:01 +0530

    updated bat file

--- bat_file.bat
+++ bat_file.bat
@@ -1,3 +1,2 @@
 C:
-cd C:/Users/sudha.kotni/AppData/Roaming/npm/node_modules/newman/bin/
-node newman -c tests.json -e env.json -g globals.json -H output.html
+node C:/Users/sudha.kotni/AppData/Roaming/npm/node_modules/newman/bin/newman -c tests.json -e env.json -g globals.json -H output.html
